f1bad65c3d4356d7abd35195d84219734f4533f7
Medhansh Verma <medhanshv03@gmail.com>
2023-09-24 03:51:43 +0530

    add

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>
+        Weather App for Omnify
+      </title>
       <body>
         <Main />
         <NextScript />
